921c6f678926c80036dcd05a5a20df572de1e1da
Sebastian Sangervasi <villain@harmless.dev>
2024-01-15 16:42:23 -0800

    layout

--- podbast-app/src/features/layout/index.tsx
+++ podbast-app/src/features/layout/index.tsx
@@ -1,13 +1,25 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Center } from '@chakra-ui/react'
 
 import { Player } from '/src/features/player'
 import { Rss } from '/src/features/rss'
 
-export const Layout = () => {
-	return (
-		<Box>
-			<Rss />
-			<Player />
-		</Box>
-	)
-}
+export const Top = () => <Box bg="gray.100" />
+
+export const Side = () => <Box bg="gray.200" />
+
+export const Main = () => (
+	<Center paddingY={10}>
+		<Rss />
+	</Center>
+)
+
+export const Bottom = () => <Player />
+
+export const Layout = () => (
+	<Box>
+		<Top />
+		<Side />
+		<Main />
+		<Bottom />
+	</Box>
+)
